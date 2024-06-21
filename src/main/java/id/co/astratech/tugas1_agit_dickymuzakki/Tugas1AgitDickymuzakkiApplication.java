2b0a5913cb5e72c48e290b7a70b594762f7e8155
student <hilmyarjuna@gmail.com>
2024-06-21 14:11:22 +0700

    tugas 1

---
+++ src/main/java/id/co/astratech/tugas1_agit_dickymuzakki/Tugas1AgitDickymuzakkiApplication.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_dickymuzakki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1AgitDickymuzakkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas1AgitDickymuzakkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/astratech/tugas1_agit_dickymuzakki/Tugas1AgitDickymuzakkiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.astratech.tugas1_agit_dickymuzakki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1AgitDickymuzakkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
